21162239abc6d8405d6682e62a3a27768682371a
leonguyen <nam85it@gmail.com>
2013-05-27 16:58:14 +0700

    update

--- gen/com/example/antispam/R.java
+++ gen/com/example/antispam/R.java
@@ -23,8 +23,11 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int bg=0x7f020001;
-        public static final int bg_button=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int color_button=0x7f020002;
+        public static final int color_list=0x7f020003;
+        public static final int color_list_pressed=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int list=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f08000d;
